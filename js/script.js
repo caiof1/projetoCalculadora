2d64ce6a0b0743cd177fe9987706771c688f36ac
Caio Fernandes <caionl2071@gmail.com>
2023-02-16 13:19:27 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
+// Essa função recebe numero ou sinal, e adiciona na screen
 function Calc(n) {
     let screen = document.getElementById('screen')
     screen.value += n
 }
 
+// Esse função dá o resultado, pegando a equação da tela, e usando a função eval para transformar a string em uma conta
 function Res() {
     let screen = document.getElementById('screen')
     screen.value = eval(screen.value)
 }
 
+// Esse função serve para apagar o ultimo numero digitado. Acredito que não seja a forma mais profissional, mas, foi a forma que eu achei de resolver
 function Apagar() {
     let screen = document.getElementById('screen')
+    // Estou adicinando os numeros da tela, para um arr, fragmentando ele
     let arr = [...screen.value]
     screen.value = ''
+    // Esse laço pega o arr, e percorre ele - 1, adicionando todos os numeros novamente na screen, e retirando apenas o ultimo
     for(let i = 0; i < arr.length - 1; i++) {
         screen.value += arr[i]
     }
